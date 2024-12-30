89b70db12f421c95e6f536e1a547e3f4921b81fa
Fhadshnde <Fhadshnde32@gmail.com>
2024-12-30 04:01:52 +0300

    add

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT || 8800, () => {
   console.log(process.env.MONGO_URL )
   console.log("Backend server is running!");
 });
