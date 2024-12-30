7a4d11bbf991086d7d76dbf6dd28d438a6b9b9f8
Fhadshnde <Fhadshnde32@gmail.com>
2024-12-30 06:55:21 +0300

    Fixed 3

--- routes/users.js
+++ routes/users.js
@@ -40,15 +40,12 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+//GET USER
 router.get("/:id", async (req, res) => {
-  const userId = req.query.userId;
-  const username = req.query.username;
   try {
-    const user = userId
-      ? await User.findById(userId)
-      : await User.findOne({ username: username });
-    const { password, updatedAt, ...other } = user._doc;
-    res.status(200).json(other);
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
   }
