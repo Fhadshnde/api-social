b237348addb50c7f09458635386e98e727bd244c
Fhadshnde <Fhadshnde32@gmail.com>
2024-12-30 06:52:57 +0300

    Fixed 2

--- routes/users.js
+++ routes/users.js
@@ -40,7 +40,7 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/:id", async (req, res) => {
   const userId = req.query.userId;
   const username = req.query.username;
   try {
